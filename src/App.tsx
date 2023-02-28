3021e8b693842789d9028e3c3f2d0c029898a7c8
Luis Alberto <luis.alberto@dspot.pl.com>
2023-02-28 02:36:39 -0500

    Add main and app files

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React, {useState} from "react";
+import BasicTaskList from "./components/BasicTaskList";
+import AggridList from "./components/AggridList";
+
+function App() {
+
+    return (
+        <div className="App">
+            <BasicTaskList/>
+        </div>
+    )
+}
+
+export default App
