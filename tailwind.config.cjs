14d015d804b6aee63ae6d52246bad1e9b91e1a38
Luis Alberto <luis.alberto@dspot.pl.com>
2023-03-13 11:53:48 -0400

    small changes in configuration

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,5 +1,7 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+const withMT = require("@material-tailwind/react/utils/withMT");
+
+module.exports =withMT({
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
@@ -9,4 +11,4 @@ module.exports = {
     },
   },
   plugins: [],
-}
+});
