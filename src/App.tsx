5ee61627d635ff109dfcae1054679c0f7b179272
Luis Alberto <luis.alberto@dspot.pl.com>
2023-03-13 11:54:15 -0400

    update app

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
-import React, {useState} from "react";
-import BasicTaskList from "./components/BasicTaskList";
-import AggridList from "./components/AggridList";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { useState } from "react";
+import AuthForm  from "./components/Login/AuthForm";
+import BasicTaskList from "./components/TaskMenu/TaskList";
 
-function App() {
+export default function App() {
+  const [auth, setAuth] = useState<boolean>(false);
 
-    return (
-        <div className="App">
-            <BasicTaskList/>
-        </div>
-    )
+  const queryClient = new QueryClient();
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <div className="App">
+        {auth ? <BasicTaskList /> : <AuthForm auth={setAuth}/>}
+      </div>
+      <ReactQueryDevtools />
+    </QueryClientProvider>
+  );
 }
-
-export default App
