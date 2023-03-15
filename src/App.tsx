e56a1d17ee67dfb17eaa2390c8d98f5406785c11
Luis Alberto <luis.alberto@dspot.pl.com>
2023-03-15 11:31:17 -0400

    add security

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { useState } from "react";
-import AuthForm  from "./components/Login/AuthForm";
+import AuthForm from "./components/Login/AuthForm";
 import BasicTaskList from "./components/TaskMenu/TaskList";
 
 export default function App() {
@@ -12,7 +12,7 @@ export default function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <div className="App">
-        {auth ? <BasicTaskList /> : <AuthForm auth={setAuth}/>}
+        {auth ? <BasicTaskList /> : <AuthForm auth={setAuth} />}
       </div>
       <ReactQueryDevtools />
     </QueryClientProvider>
